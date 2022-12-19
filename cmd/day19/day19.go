95fab334cdda5f92dffd36b11e93d22c8a6ef785
Mario Liebisch <mario.liebisch@gmail.com>
2022-12-19 10:24:14 +0100

    Day 19 (PH)

---
+++ cmd/day19/day19.go
@@ -0,0 +1,21 @@
+package main
+
+const sample = "./data/sample-19.txt"
+const input = "./data/input-19.txt"
+
+func solve1(file string) int {
+	var res = 0
+	return res
+}
+
+func solve2(file string) int {
+	var res = 0
+	return res
+}
+
+func main() {
+	println("Solution 1 - Sample:", solve1(sample))
+	println("Solution 1 - Input:", solve1(input))
+	println("Solution 2 - Sample:", solve2(sample))
+	println("Solution 2 - Input:", solve2(input))
+}
